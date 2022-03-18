5bd47f49169031934a7ad9cf44f0ae6787f2e1aa
Алексей <alexsynarchin@gmail.com>
2022-03-18 11:44:14 +0500

    Admin panel

--- resources/admin/js/views/home/index.vue
+++ resources/admin/js/views/home/index.vue
@@ -1,5 +1,145 @@
 <template>
+    <section>
+        <div class="row mb-4">
+            <div class="col-md-4">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>Доход</span>
+                    </div>
+                    <div class="mp-card__sum">
+                        0 руб
+                    </div>
+                    <div class="mp-card-today">
+                        <label class="mp-card-today__label">
+                            За сегодня:
+                        </label>
+                        <span class="mp-card-today__value">
+                        0 руб
+                    </span>
+                    </div>
 
+                </el-card>
+            </div>
+
+            <div class="col-md-4">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix">
+                        <span>Пользователи</span>
+                    </div>
+                    <div class="mp-card__sum">
+                        0
+                    </div>
+                    <div class="mp-card-today">
+                        <label class="mp-card-today__label">
+                            За сегодня:
+                        </label>
+                        <span class="mp-card-today__value">
+                        0
+                    </span>
+                    </div>
+
+                </el-card>
+            </div>
+        </div>
+       <div class="row">
+           <div class="col-md-3 mb-3">
+               <el-card class="box-card mb-2">
+                   <div slot="header" class="clearfix">
+                       <span>Заявки на рерайт</span>
+                   </div>
+                   <div class="mp-card__sum">
+                       0
+                   </div>
+                   <div class="mp-card-today">
+                       <label class="mp-card-today__label">
+                           За сегодня:
+                       </label>
+                       <span class="mp-card-today__value">
+                        0
+                    </span>
+                   </div>
+
+               </el-card>
+               <el-card class="box-card">
+                   <div slot="header" class="clearfix">
+                       <span>Последние</span>
+                   </div>
+               </el-card>
+           </div>
+           <div class="col-md-3 mb-3">
+               <el-card class="box-card mb-2">
+                   <div slot="header" class="clearfix">
+                       <span>Бесплатные заявки</span>
+                   </div>
+                   <div class="mp-card__sum">
+                       0
+                   </div>
+                   <div class="mp-card-today">
+                       <label class="mp-card-today__label">
+                           За сегодня:
+                       </label>
+                       <span class="mp-card-today__value">
+                        0
+                    </span>
+                   </div>
+
+               </el-card>
+               <el-card class="box-card">
+                   <div slot="header" class="clearfix">
+                       <span>Последние</span>
+                   </div>
+               </el-card>
+           </div>
+           <div class="col-md-3 mb-3">
+               <el-card class="box-card mb-2">
+                   <div slot="header" class="clearfix">
+                       <span>Автоматические заявки</span>
+                   </div>
+                   <div class="mp-card__sum">
+                       0
+                   </div>
+                   <div class="mp-card-today">
+                       <label class="mp-card-today__label">
+                           За сегодня:
+                       </label>
+                       <span class="mp-card-today__value">
+                        0
+                    </span>
+                   </div>
+
+               </el-card>
+               <el-card class="box-card">
+                   <div slot="header" class="clearfix">
+                       <span>Последние</span>
+                   </div>
+               </el-card>
+           </div>
+           <div class="col-md-3 mb-3">
+               <el-card class="box-card mb-2">
+                   <div slot="header" class="clearfix">
+                       <span>Ручные заявки</span>
+                   </div>
+                   <div class="mp-card__sum">
+                       0
+                   </div>
+                   <div class="mp-card-today">
+                       <label class="mp-card-today__label">
+                           За сегодня:
+                       </label>
+                       <span class="mp-card-today__value">
+                        0
+                    </span>
+                   </div>
+
+               </el-card>
+               <el-card class="box-card">
+                   <div slot="header" class="clearfix">
+                       <span>Последние</span>
+                   </div>
+               </el-card>
+           </div>
+       </div>
+    </section>
 </template>
 <script>
     export default {
