eedd8e890122704c11f70110f8535b4835a7ecbb
Алексей <alexsynarchin@gmail.com>
2022-04-05 14:30:17 +0500

    Main page

--- resources/views/site/home/_partials/reviews.blade.php
+++ resources/views/site/home/_partials/reviews.blade.php
@@ -1,5 +1,24 @@
 <section class="mp-reviews">
     <div class="container">
+        <div class="mp-reviews__heading">
+            <h3 class="mp-reviews__title block-title">
+                Что говорят наши клиенты
+            </h3>
+            <div class="mp-reviews__controls">
+                <figure class="mp-reviews-control">
+                    <svg class="mp-reviews-control__icon" viewBox="0 0 13 13">
+                        <use xlink:href="assets/site/images/sprites.svg?ver=15#sprite-arrow-left"></use>
+                    </svg>
+                </figure>
+                <figure class="mp-reviews-control">
+                    <svg class="mp-reviews-control__icon" viewBox="0 0 13 13">
+                        <use xlink:href="assets/site/images/sprites.svg?ver=15#sprite-arrow-right"></use>
+                    </svg>
+                </figure>
+
+            </div>
+        </div>
+
         <div class="mp-reviews-list owl-carousel owl-theme">
             <div class="mp-reviews-list__item">
                 <h3 class="mp-reviews-list__title">
