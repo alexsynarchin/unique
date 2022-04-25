ca7a167d5960a8f17333981abca5ad55c491475c
Алексей <alexsynarchin@gmail.com>
2022-04-25 12:35:45 +0500

    faq fix

--- resources/site/js/components/accordion/AccordionItem.vue
+++ resources/site/js/components/accordion/AccordionItem.vue
@@ -1,21 +1,36 @@
 <template>
     <section class="faq-item">
-        <div class="faq-item__question">
+        <div class="faq-item__question" @click.prevent="handleAnswer">
             <h3 class="faq-item__question-text"><slot name="question"></slot></h3>
             <figure class="faq-item__question-icon">
-                <svg viewBox="0 0 15 15">
+                <svg viewBox="0 0 15 15" v-if="AnswerVisible">
+                    <use xlink:href="/assets/site/images/sprites.svg?ver=15#sprite-minus"></use>
+                </svg>
+                <svg viewBox="0 0 15 15" v-else>
                     <use xlink:href="/assets/site/images/sprites.svg?ver=15#sprite-plus"></use>
                 </svg>
             </figure>
-
         </div>
-        <div class="faq-item__answer">
+        <div class="faq-item__answer"
+            :class="{
+            'faq-item__answer--open':AnswerVisible
+            }"
+        >
             <slot name="answer"></slot>
         </div>
     </section>
 </template>
 <script>
 export default {
-
+    data() {
+        return {
+            AnswerVisible:false,
+        }
+    },
+     methods: {
+        handleAnswer() {
+          this.AnswerVisible = !this.AnswerVisible;
+        },
+     }
 }
 </script>
