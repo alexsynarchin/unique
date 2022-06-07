325345c5bae87c6bfa9c29354e11425acdddea6f
Алексей <alexsynarchin@gmail.com>
2022-06-07 10:46:59 +0500

    smal fixes

--- resources/site/js/views/faq-page/index.vue
+++ resources/site/js/views/faq-page/index.vue
@@ -9,38 +9,19 @@ export default {
     },
     data() {
         return {
-            faqList: [
-                {
-                    question: "Как проверить работу на антиплагиат?",
-                    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget dolor lacinia, semper dolor aliquet, luctus neque. Sed sagittis lacus nunc, eu vestibulum elit dignissim id. Donec luctus ornare elit, a elementum erat maximus in. Suspendisse non nunc vel eros consectetur aliquam vel placerat quam. Integer et urna scelerisque, laoreet quam mattis, eleifend tellus. Nunc et arcu at sapien suscipit facilisis. Mauris congue vel arcu id vehicula."
-                },
-                {
-                    question: "Как происходит сам процесс оформления заказа?",
-                    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget dolor lacinia, semper dolor aliquet, luctus neque. Sed sagittis lacus nunc, eu vestibulum elit dignissim id. Donec luctus ornare elit, a elementum erat maximus in. Suspendisse non nunc vel eros consectetur aliquam vel placerat quam. Integer et urna scelerisque, laoreet quam mattis, eleifend tellus. Nunc et arcu at sapien suscipit facilisis. Mauris congue vel arcu id vehicula."
-
-                },
-                {
-                    question: "Сколько времени занимает проверка? ",
-                    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget dolor lacinia, semper dolor aliquet, luctus neque. Sed sagittis lacus nunc, eu vestibulum elit dignissim id. Donec luctus ornare elit, a elementum erat maximus in. Suspendisse non nunc vel eros consectetur aliquam vel placerat quam. Integer et urna scelerisque, laoreet quam mattis, eleifend tellus. Nunc et arcu at sapien suscipit facilisis. Mauris congue vel arcu id vehicula."
-
-                },
-                {
-                    question: "Вы занимаетесь рерайтом?",
-                    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget dolor lacinia, semper dolor aliquet, luctus neque. Sed sagittis lacus nunc, eu vestibulum elit dignissim id. Donec luctus ornare elit, a elementum erat maximus in. Suspendisse non nunc vel eros consectetur aliquam vel placerat quam. Integer et urna scelerisque, laoreet quam mattis, eleifend tellus. Nunc et arcu at sapien suscipit facilisis. Mauris congue vel arcu id vehicula."
-
-                },
-                {
-                    question: "Как проверить работу на антиплагиат? Вы можете мне в этом помочь?",
-                    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget dolor lacinia, semper dolor aliquet, luctus neque. Sed sagittis lacus nunc, eu vestibulum elit dignissim id. Donec luctus ornare elit, a elementum erat maximus in. Suspendisse non nunc vel eros consectetur aliquam vel placerat quam. Integer et urna scelerisque, laoreet quam mattis, eleifend tellus. Nunc et arcu at sapien suscipit facilisis. Mauris congue vel arcu id vehicula."
-
-                },
-                {
-                    question: "Как проверить работу на антиплагиат?",
-                    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget dolor lacinia, semper dolor aliquet, luctus neque. Sed sagittis lacus nunc, eu vestibulum elit dignissim id. Donec luctus ornare elit, a elementum erat maximus in. Suspendisse non nunc vel eros consectetur aliquam vel placerat quam. Integer et urna scelerisque, laoreet quam mattis, eleifend tellus. Nunc et arcu at sapien suscipit facilisis. Mauris congue vel arcu id vehicula."
-
-                },
-            ],
+            faqList: [],
         }
     },
+    methods: {
+        getList() {
+            axios.get('/api/block-list/faq')
+                .then((response) => {
+                    this.prices = response.data;
+                })
+        }
+    },
+    mounted() {
+        this.getList();
+    }
 }
 </script>
