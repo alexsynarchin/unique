a47f48cad417f9f5ec5252ba7166212b3544569f
Алексей <alexsynarchin@gmail.com>
2022-07-14 15:23:03 +0500

    fix bugs

--- app/Http/Controllers/Site/Api/CheckUniqueController.php
+++ app/Http/Controllers/Site/Api/CheckUniqueController.php
@@ -49,8 +49,9 @@ class CheckUniqueController extends Controller
         //dd($request->file('file')->getClientMimeType());
         if($request->file('file')->getClientMimeType() === 'application/msword') {
             //dd($request->file('file')->getRealPath());
-            $text = shell_exec('/usr/bin/antiword'.$request->file('file')->getRealPath());
-            dd($text);
+            $filename = $request->file('file')->getRealPath();
+            $text = shell_exec('/usr/bin/antiword -X UTF-8 '.$filename);
+
         }  elseif ($request->file('file')->getClientMimeType() === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
             $objReader = \PhpOffice\PhpWord\IOFactory::createReader('Word2007');
 
