dcef99bb1b0b509ef2f695ff20adcdab504011a3
Алексей <alexsynarchin@gmail.com>
2022-07-14 14:56:40 +0500

    fix bugs

--- app/Http/Controllers/Site/Api/CheckUniqueController.php
+++ app/Http/Controllers/Site/Api/CheckUniqueController.php
@@ -50,7 +50,7 @@ class CheckUniqueController extends Controller
         if($request->file('file')->getClientMimeType() === 'application/msword') {
             $text = (new Doc())
                 ->setDoc($source)
-                ->setOptions(['X UTF-8 '])
+                ->setOptions(['X UTF-8'])
                 ->text();
         }  elseif ($request->file('file')->getClientMimeType() === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
             $objReader = \PhpOffice\PhpWord\IOFactory::createReader('Word2007');
