da3d993cb0c93022be3ef0b7eeac7e807dfc05ca
Алексей <alexsynarchin@gmail.com>
2022-07-16 23:58:42 +0500

    fix bugs

--- resources/admin/js/views/main-page/components/utp.vue
+++ resources/admin/js/views/main-page/components/utp.vue
@@ -1,13 +1,23 @@
 <template>
     <section class="page-block">
+        <el-form ref="form">
+            <el-form-item label="Заголовок">
+                <el-input v-model="form.title"></el-input>
+            </el-form-item>
+            <el-form-item label="Описание">
 
+            </el-form-item>
+        </el-form>
     </section>
 </template>
 <script>
     export default {
         data() {
             return {
-
+                form: {
+                    title: "",
+                    description: "",
+                }
             }
         }
     }
