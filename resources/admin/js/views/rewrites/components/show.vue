a4d20f88bfc91b4a0b2b80732717052bf4aadafa
Alex <alexsynarchin@gmail.com>
2022-07-31 19:45:18 +0500

    Rewrites orders

--- resources/admin/js/views/rewrites/components/show.vue
+++ resources/admin/js/views/rewrites/components/show.vue
@@ -1,8 +1,23 @@
 <template>
-
+    <section>
+        <div class="row">
+            <div class="col-md-6">
+                <label>Имя</label>
+            </div>
+        </div>
+    </section>
 </template>
 <script>
     export default {
+        data() {
+            return {
+                item: {}
+            }
+        },
+        methods: {
+            getData() {
 
+            },
+        }
     }
 </script>
