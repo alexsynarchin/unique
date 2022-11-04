7682053c70678070a84f6b3843a4593e548f5418
Alex <alexsynarchin@gmail.com>
2022-11-04 12:47:07 +0500

    video modal size

--- resources/site/js/components/modals/video-modal/modal.vue
+++ resources/site/js/components/modals/video-modal/modal.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="modal fade unique-modal" id="video_modal"  tabindex="-1" role="dialog"  aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered unique-modal__dialog" role="document">
+    <div class="modal fade unique-modal video-modal" id="video_modal"  tabindex="-1" role="dialog"  aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered unique-modal__dialog video-modal__dialog" role="document">
             <div class="unique-modal__content modal-content video-modal__content">
                 <button  class="unique-modal__close" type="button"  data-bs-dismiss="modal" aria-label="Close">
                     <svg viewBox="0 0 41 40">
                         <use xlink:href="assets/site/images/sprites.svg?ver=44#sprite-close-gray"></use>
                     </svg>
                 </button>
-                <iframe width="100%" height="315" :src="url" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                <iframe width="100%" height="auto" :src="url" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
         </div>
     </div>
     </div>
