252346da1c3960349946523b7307b97634e5c993
Alex <alexsynarchin@gmail.com>
2022-11-30 15:10:27 +0500

    Favicon

--- resources/views/site/base/_partials/head.blade.php
+++ resources/views/site/base/_partials/head.blade.php
@@ -5,7 +5,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="{{mix('css/app.css', 'assets/site')}}?ver=19" rel="stylesheet" type="text/css">
-    <link rel="shortcut icon" href="{{URL::asset('images/favicon.png')}}" type="image/png">
+    <link rel="shortcut icon" href="{{URL::asset('assets/site/images/favicon.png')}}" type="image/png">
     <meta name="csrf-token" content="{{ csrf_token()}}" />
     <meta name="description" content="">
     <title>Проверка уникальности</title>
