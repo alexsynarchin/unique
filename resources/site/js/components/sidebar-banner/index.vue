646b359a73086e35d2928e144544066246308b43
Alex <alexsynarchin@gmail.com>
2022-12-26 15:22:39 +0500

    download report if report result

--- resources/site/js/components/sidebar-banner/index.vue
+++ resources/site/js/components/sidebar-banner/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <a href="" class="sidebar-banner">
+    <a href="https://vk.com/share.php?url=https://xn----8sbempbojoebkbodzijk2phe.xn--p1ai" class="sidebar-banner">
         <img src="/assets/site/images/sidebar-banner.png">
     </a>
 </template>
