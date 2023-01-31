48cd2cb93a6477b8ab53603af0d0bff020a65c79
Alex <alexsynarchin@gmail.com>
2023-01-31 19:39:59 +0500

    Api Accounts

--- resources/views/site/articles/show.blade.php
+++ resources/views/site/articles/show.blade.php
@@ -36,6 +36,9 @@
                     </a>
                     <button class="button btn vk-subscribe__btn">Подписаться</button>
                 </section>
+                <p>Поделиться статьей:</p>
+                <script src="https://yastatic.net/share2/share.js"></script>
+                <div class="ya-share2" data-curtain data-services="vkontakte,odnoklassniki"></div>
             </div>
         </div>
         <div class="row mb-5">
