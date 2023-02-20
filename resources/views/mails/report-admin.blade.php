c2fde568ed4bc40f988ad57e9bd9ef2a48b2955d
Alex <alexsynarchin@gmail.com>
2023-02-20 12:38:31 +0500

    Email fix

--- resources/views/mails/report-admin.blade.php
+++ resources/views/mails/report-admin.blade.php
@@ -5,6 +5,12 @@
     @foreach($order->reports as $report)
         {{$report->checkSystem->title}}@if(!$loop->last),@endif
     @endforeach<br>
+<label>Дополнительные услуги:</label>
+@foreach($order->checkUnique->services as $service)
+    {{$service->title}}
+    @if(!$loop->last),@endif
+@endforeach
+<br>
 <label>Страница запроса в админ панели:</label>
 <a href="https://xn----8sbempbojoebkbodzijk2phe.xn--p1ai/admin/check-uniques/show/{{$order->checkUnique->id}}">
     Перейти
