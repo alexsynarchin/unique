c1a01f740d2d9e359f68dfca77912798383c195f
Alex <alexsynarchin@gmail.com>
2023-03-14 19:47:52 +0500

    symbols count in text 500000

--- resources/site/js/components/repost/index.vue
+++ resources/site/js/components/repost/index.vue
@@ -91,21 +91,7 @@
 
                 VK.Auth.login(function(response) {
                     console.log(response)
-                    if (response.session) {
-                        /* Пользователь успешно авторизовался */
 
-                        VK.Api.call('wall.post', {
-                            owner_id:response.session.mid,
-                            message:'Проверка-уникальности. Бесплатная проверка уникальности текста. Все системы в одном месте',
-                        }, function (r) {
-                            console.log(r.response.post_id)
-                            if(r.response.post_id) {
-                                bus.$emit('show-promo-modal');
-                            }
-                        });
-                    } else {
-
-                    }
                 });
             },
         },
