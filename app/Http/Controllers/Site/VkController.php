783ee93f8930f2c77e46a09bc67776476635ee4a
Alex <alexsynarchin@gmail.com>
2023-03-14 17:32:04 +0500

    symbols count in text 500000

--- app/Http/Controllers/Site/VkController.php
+++ app/Http/Controllers/Site/VkController.php
@@ -13,7 +13,7 @@ class VkController extends Controller
         $oauth = new \VK\OAuth\VKOAuth();
         $client_id = 51553840;
         $client_secret = '45uuDrTz0Pcia2fhSwBy';
-        $redirect_uri = 'http://unique.loc/vk';
+        $redirect_uri = 'https://%D0%BF%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D0%B8%D1%82%D1%8C-%D1%83%D0%BD%D0%B8%D0%BA%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C.%D1%80%D1%84/vk';
         $code = $request->get('code');
 
         $response = $oauth->getAccessToken($client_id, $client_secret, $redirect_uri, $code);
