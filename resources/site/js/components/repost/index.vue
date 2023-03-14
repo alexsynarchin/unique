ac009a44c5be2d61a4e404ca810bd2a3eb605093
Alex <alexsynarchin@gmail.com>
2023-03-14 20:52:31 +0500

    symbols count in text 500000

--- resources/site/js/components/repost/index.vue
+++ resources/site/js/components/repost/index.vue
@@ -110,7 +110,7 @@
             setTimeout(function() {
                 var el = document.createElement("script");
                 el.type = "text/javascript";
-                el.src = "/assets/site/js/openapi.js";
+                el.src = "https://vk.com/js/api/openapi.js?169";
                 el.async = true;
                 document.getElementById("vk_api_transport").appendChild(el);
 
