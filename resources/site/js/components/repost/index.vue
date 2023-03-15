39307e32d12b30fd7815dda99def9041a8b07519
Alex <alexsynarchin@gmail.com>
2023-03-15 08:52:47 +0500

    symbols count in text 500000

--- resources/site/js/components/repost/index.vue
+++ resources/site/js/components/repost/index.vue
@@ -111,7 +111,7 @@
             },
             sendRepost() {
                 let vm = this;
-                VK.Auth.getSession(function(response) {
+                VK.Auth.getLoginStatus(function(response) {
                     console.log(response);
                     if (response.session) {
                         /* Пользователь успешно авторизовался */
