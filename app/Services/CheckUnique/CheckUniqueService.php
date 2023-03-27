87b006004f64d5ae88235c05c738c666c97f9870
Alex <alexsynarchin@gmail.com>
2023-03-27 12:49:51 +0500

    Check Unique paid protect

--- app/Services/CheckUnique/CheckUniqueService.php
+++ app/Services/CheckUnique/CheckUniqueService.php
@@ -17,7 +17,7 @@ class CheckUniqueService
 
     public function getResult($uid)
     {
-        $userkey = '11a38b8038a81e4544ccd22b2259411a';
+        $userkey = '2c58d49fbb37631e5867ada0dd394f66';
         sleep(15);
         //Получаете результат проверки
         $jsonvisible = 'detail'; //Необязательный параметр. Укажите "detail" чтобы получить расширенные данные по тексту
