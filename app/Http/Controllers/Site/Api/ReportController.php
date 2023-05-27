3cf69a817086ca4be8693365c9b1d9f5e330f07a
Alex <alexsynarchin@gmail.com>
2023-05-27 13:44:55 +0500

    Send status free check uniques

--- app/Http/Controllers/Site/Api/ReportController.php
+++ app/Http/Controllers/Site/Api/ReportController.php
@@ -73,6 +73,7 @@ class ReportController extends Controller
         $send_status = false;
         if(Setting::where('group', 'common') -> where('name', 'free_email_send') -> exists()) {
             $send_setting = Setting::where('group', 'common') -> where('name', 'free_email_send')->first();
+            $send_setting = $send_setting -> value;
             if( ((int) $send_setting) === 1) {
                 $send_status = true;
             }
