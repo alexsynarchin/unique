bf23699dee248d8fd150a3a3e504f985aa87875b
Alex <alexsynarchin@gmail.com>
2023-06-19 14:42:05 +0500

    fix show check unique query

--- app/Http/Controllers/Site/CheckReportController.php
+++ app/Http/Controllers/Site/CheckReportController.php
@@ -11,11 +11,13 @@ class CheckReportController extends Controller
 {
     public function show($slug)
     {
-        $check_unique = CheckUnique::where('slug', $slug)->firstOrFail();
+        $check_unique = CheckUnique::where('slug', $slug)->firstOrFail(['id']);
 
         //vk_app_51581157
       // Cookie::queue('vk_app_51554125','',10);
         //Cookie::queue(Cookie::forget('vk_app_51581291'));
         return view('site.check-report.show', ['id' => $check_unique->id]);
     }
+
+
 }
