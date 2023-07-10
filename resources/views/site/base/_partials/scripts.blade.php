a2fe7346e17cc2fb114d694741155bc7b6f7e472
Alex <alexsynarchin@gmail.com>
2023-07-10 15:12:56 +0500

    fix show check unique query

--- resources/views/site/base/_partials/scripts.blade.php
+++ resources/views/site/base/_partials/scripts.blade.php
@@ -30,3 +30,7 @@
     {!!  GoogleReCaptchaV3::init() !!}
 @endif
 
+<script type="text/javascript">!function(){var
+        t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1735342-       8hgul"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img
+        src="https://vk.com/rtrg?p=VK-RTRG-1735342-8hgul" style="position:fixed; left:-999px;"
+        alt=""/></noscript>
