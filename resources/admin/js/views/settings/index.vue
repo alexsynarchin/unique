e0d5dfdab2aeb4bc6be3d9bf36543d75d0adb214
Alex <alexsynarchin@gmail.com>
2023-10-19 01:25:53 +0500

    Setting for repost link in socila media. Make open graph microdata editable.

--- resources/admin/js/views/settings/index.vue
+++ resources/admin/js/views/settings/index.vue
@@ -31,7 +31,7 @@ export default {
     data() {
         return {
             CanAccess: 'Просмотр "Настройки"',
-            activeTab: 'repost'
+            activeTab: 'common'
         };
     },
     methods: {
