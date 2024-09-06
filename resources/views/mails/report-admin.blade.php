0dbc1ce9d5d47cffae33757f10ea7b7dae554dc5
Alexey <alexsynarchin@gmail.com>
2024-09-06 11:38:58 +0500

    Order currency

--- resources/views/mails/report-admin.blade.php
+++ resources/views/mails/report-admin.blade.php
@@ -1,6 +1,6 @@
 <h4>Запрос №{{$order->checkUnique->id}}</h4>
 <label>Почта: </label> {{$order->checkUnique->email}}<br>
-<label>Сумма: </label> {{$order->sum}} руб.<br>
+<label>Сумма: </label> {{$order->sum}} {{$order->currency}}<br>
 <label>Системы проверки: </label>
     @foreach($order->reports as $report)
         {{$report->checkSystem->title}}@if(!$loop->last),@endif
