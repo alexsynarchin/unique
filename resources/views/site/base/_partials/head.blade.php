dc1855a966582bd62d10ea4af1c7242d8673c9d4
Alexey <alexsynarchin@gmail.com>
2024-09-06 13:19:34 +0500

    LightSpeed optimaze

--- resources/views/site/base/_partials/head.blade.php
+++ resources/views/site/base/_partials/head.blade.php
@@ -1,10 +1,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href="{{mix('css/app.css', 'assets/site')}}?ver=19" rel="stylesheet" type="text/css">
+
+    <link href="{{mix('css/app.css', 'assets/site')}}" rel="stylesheet" type="text/css">
     <link rel="shortcut icon" href="{{URL::asset('assets/site/images/favicon.png')}}" type="image/png">
     <meta name="csrf-token" content="{{ csrf_token()}}" />
     <title>@if(isset($seo['title'])){{$seo['title']?$seo['title']:$mp_seo -> title}}@else{{$mp_seo -> title}}@endif</title>
