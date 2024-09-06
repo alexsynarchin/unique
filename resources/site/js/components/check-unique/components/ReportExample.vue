9e72b13349530713d8b2724282ca9aaa06ee1354
Alexey <alexsynarchin@gmail.com>
2024-09-06 19:35:39 +0500

    LightSpeed optimaze

--- resources/site/js/components/check-unique/components/ReportExample.vue
+++ resources/site/js/components/check-unique/components/ReportExample.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="modal unique-modal" id="report_example"
-         tabindex="-1" role="dialog"  aria-hidden="true" data-bs-backdrop="static">
+         tabindex="-1" role="dialog"  aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered unique-modal__dialog" role="document">
             <div class="unique-modal__content modal-content">
                 <button  class="unique-modal__close" type="button"  data-bs-dismiss="modal" aria-label="Close">
@@ -11,9 +11,11 @@
                 <h3 class="unique-modal__title">
                     Пример отчета
                 </h3>
-                <object :data="url" type="application/pdf" width="100%" height="500px">
-                    <p><b>Упс! Кажется, PDF-файл не отобразился.</b> Попробуйте скачать его <a href="url">отсюда</a>.</p>
+                <object :data="url" type="application/pdf" frameborder="0" width="100%" height="600px" style="padding: 20px;">
+                    <embed :src="url" width="100%" height="600px"/>
                 </object>
+
+
 
 
             </div>
