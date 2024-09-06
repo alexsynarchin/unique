3000b04b84215c0a9082d1a33b3902c79c244654
Alexey <alexsynarchin@gmail.com>
2024-09-06 12:23:56 +0500

    LightSpeed optimaze

--- webpack.mix.frontend.js
+++ webpack.mix.frontend.js
@@ -3,7 +3,7 @@ let distPath = 'public/assets/site';
 const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
 
 mix
-    .extract(['vue', 'jquery'])
+    .extract(['vue', 'jquery', 'bootstrap'])
     .js('resources/site/js/app.js', 'js')
     .vue({ version: 2 })
     .sass('resources/site/scss/app.scss', 'css')
