0260040ac52b447874a8990306f30b614a6c79d2
Alexey <alexsynarchin@gmail.com>
2025-01-20 12:52:13 +0500

    Retainer check api add

--- resources/site/js/views/report/components/report-detail.vue
+++ resources/site/js/views/report/components/report-detail.vue
@@ -57,7 +57,7 @@
         },
         data() {
             return {
-                showDetail: false,
+                showDetail: true,
                 textArray:[],
                 resultText: "",
             }
