32be4d7e1b9b5d0222bdeb6f99bc10b2bbf91472
Alexey <alexsynarchin@gmail.com>
2025-01-27 18:01:50 +0500

    Rewrite email fix

--- resources/views/mails/rewrite-order.blade.php
+++ resources/views/mails/rewrite-order.blade.php
@@ -3,8 +3,8 @@
 <label>Почта:</label>{{$contact->email}}<br>
 <label>Необходимые сроки:</label>{{$contact->date}}<br>
 <label>Комментарий:</label><br>
+{{$contact->comment}}
 @if(!$contact->filename)
 <label>Текст:</label><br>
 {{$contact->plain_text}}<br>
-{{$contact->comment}}
 @endif
