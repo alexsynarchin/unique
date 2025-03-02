0e706c449a9d37ef335caf53ce867556ad7ad909
Alexey <alexsynarchin@gmail.com>
2025-03-02 21:24:56 +0500

    test highlight text ru

--- app/Services/ReportHighLightTextService.php
+++ app/Services/ReportHighLightTextService.php
@@ -87,8 +87,8 @@ class ReportHighLightTextService
         }
         $wordsIndexesArr = explode(' ', $data['urls'][$index]['words']);
         if(isset($data['words_pos'])) {
-            dd($data);
             $text = mb_convert_encoding($data['clear_text'], "UTF-8", mb_detect_encoding($data['clear_text']));
+            dd($text);
             $textArr = $this->textToArrayTextRu($text, $data['words_pos']);
         } else {
             $textArr  = $this->textToArray($data['clear_text']);
